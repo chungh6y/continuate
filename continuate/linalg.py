e9f0e62c1e763ebc57e07efb153076d273c132e0
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-11-17 19:49:31 +0900

    Add test for linalg

--- continuate/linalg.py
+++ continuate/linalg.py
@@ -29,7 +29,7 @@ def Jacobian(func, x0, alpha=1e-7, fx=None):
     Examples
     --------
     >>> import numpy as np
-    >>> f = lambda x: np.array([x[1]^2, x[0]^2])
+    >>> f = lambda x: np.array([x[1]**2, x[0]**2])
     >>> x0 = np.array([1, 2])
     >>> J = Jacobian(f, x0)
 
