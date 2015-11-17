259e908ca0946f14b22dc97b035f4084831a5016
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-11-17 13:33:13 +0900

    Add Jacobian

---
+++ continuate/linalg.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.sparse.linalg import LinearOperator
+
+from logging import getLogger, DEBUG
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
+
+def Jacobian(func, x0, alpha=1e-7, fx=None):
+    """
+    Jacobi oprator :math:`DF(x0)`, where
+    .. math::
+        DF(x0)dx = (F(x0 + \alpha dx / |dx|) - F(x0))/(\alpha/|dx|)
+
+    Parameters
+    ----------
+    func: function
+    x0: numpy.array
+    alpha: float
+    fx: numpy.array, optional
+        func(x0)
+
+    """
+    if fx is None:
+        fx = func(x0)
+
+    def wrap(v):
+        norm = np.linalg.norm(v)
+        r = alpha / norm
+        return (func(x0 + r * v) - fx) / r
+
+    return LinearOperator((len(x0), len(x0)), matvec=wrap, dtype=x0.dtype)
