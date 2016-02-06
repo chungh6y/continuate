c01ac70296e2141ba4addbee950ac83016a1fc7a
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-06 20:03:06 +0900

    Revise tests for Krylov

--- continuate/linalg/test_krylov.py
+++ continuate/linalg/test_krylov.py
@@ -9,98 +9,101 @@ from unittest import TestCase
 
 class TestKrylov(TestCase):
 
+    def setUp(self):
+        self.N = 5
+
+    def _random_linop(self):
+        rand = np.random.rand(self.N, self.N)
+        return linalg.LinearOperator(
+            (self.N, self.N),
+            matvec=lambda x: np.dot(rand, x),
+            dtype=np.float64
+        )
+
+    def _random_vector(self):
+        return np.random.random(self.N)
+
+    def _unit_vector(self, i):
+        return np.identity(self.N)[:, i]
+
     def test_iterate_random(self):
         """ Check Arnold.iteration for random matrix
 
         Iteration must be continue until Arnoldi.H becomes square
         """
-        N = 5
-        rand = np.random.rand(N, N)
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        b = np.array([2], dtype=np.float64)
-        b.resize(N)
+        A = self._random_linop()
+        b = self._random_vector()
         O = krylov.Arnoldi(A, b)
         H = O.projected_matrix()
-        self.assertEqual(H.shape, (N, N))
+        self.assertEqual(H.shape, (self.N, self.N))
 
     def test_iterate_identity(self):
         """ Check Arnold.iteration for identity matrix
 
         Iteration does not creates Krylov subspace
         """
-        N = 5
-        A = linalg.LinearOperator((N, N), matvec=lambda x: x, dtype=np.float64)
-        b = np.array([2], dtype=np.float64)
-        b.resize(N)
+        A = linalg.aslinearoperator(np.identity(self.N))
+        b = self._unit_vector(0)
         O = krylov.Arnoldi(A, b)
         H = O.projected_matrix()
         self.assertEqual(H.shape, (1, 1))
-        Ha = np.zeros_like(H)
-        Ha[0, 0] = 1
-        np.testing.assert_equal(H, Ha)
+        np.testing.assert_equal(H[0, 0], 1)
 
     def test_basis(self):
         """ Check orthogonality of the basis """
-        N = 5
-        rand = np.random.rand(N, N)
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        b = np.array([2], dtype=np.float64)
-        b.resize(N)
-        _, V = krylov.arnoldi(A, b)
-        self.assertEqual(V.shape, (N, N))
-        v0 = np.zeros(N)
-        v0[0] = 1.0
-        np.testing.assert_equal(V[:, 0], v0)
-        for i in range(N):
+        A = self._random_linop()
+        b = self._random_vector()
+        O = krylov.Arnoldi(A, b)
+        V = O.basis()
+        self.assertEqual(V.shape, (self.N, self.N))
+        I = np.identity(self.N)
+        np.testing.assert_almost_equal(np.dot(V.T, V), I)
+        np.testing.assert_almost_equal(np.dot(V, V.T), I)
+        for i, v in enumerate(O):
+            np.testing.assert_almost_equal(V[:, i], v)
+        for i in range(self.N):
             vi = V[:, i]
             np.testing.assert_almost_equal(np.dot(vi, vi), 1.0)
-            for j in range(i + 1, N):
+            for j in range(i + 1, self.N):
                 vj = V[:, j]
                 np.testing.assert_array_almost_equal(np.dot(vi, vj), 0.0)
 
-    def test_hessenberg(self):
-        N = 5
-        rand = np.random.rand(N, N)
-        for i in range(N):
-            rand[i+1:, :i] = 0
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        b = np.zeros(N)
-        b[0] = 1
-        H, V = krylov.arnoldi(A, b)
-        np.testing.assert_almost_equal(H, rand)
-        np.testing.assert_almost_equal(V, np.identity(N))
-
-    def test_h(self):
+    def test_AV_VH(self):
         """ Confirm AV = VH """
-        N = 5
-        rand = np.random.rand(N, N)
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        b = np.random.rand(N)
-        O = krylov.Arnoldi(A, b)
-        H = O.projected_matrix()
-        V = O.basis()
-        np.testing.assert_almost_equal(V[:, 0], O[0])
-        np.testing.assert_almost_equal(np.dot(V.T, V), np.identity(N))
-        np.testing.assert_almost_equal(np.dot(V, V.T), np.identity(N))
+        A = self._random_linop()
+        b = self._random_vector()
+        H, V = krylov.arnoldi(A, b)
         np.testing.assert_almost_equal(A * V, np.dot(V, H))
 
+    def _hessenberg_matrix(self):
+        rand = np.random.rand(self.N, self.N)
+        for i in range(self.N):
+            rand[i+1:, :i] = 0
+        return rand
+
+    def test_hessenberg(self):
+        """ Case where A is Hessenberg matrix
+
+        - V becomes identity matrix when A is Hessenberg.
+        - Thus, H equals to A.
+        """
+        rand = self._hessenberg_matrix()
+        A = linalg.aslinearoperator(rand)
+        b = self._unit_vector(0)
+        H, V = krylov.arnoldi(A, b)
+        np.testing.assert_almost_equal(H, rand)
+        np.testing.assert_almost_equal(V, np.identity(self.N))
+
     def test_solve_Hessenberg(self):
-        N = 5
-        rand = np.random.rand(N, N)
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        b = np.random.rand(N)
-        O = krylov.Arnoldi(A, b)
-        H = O.projected_matrix()
-        g = krylov.solve_Hessenberg(H, 1)
-        c = np.zeros(N)
-        c[0] = 1
-        np.testing.assert_almost_equal(np.dot(H, g), c)
+        h = self._hessenberg_matrix()
+        A = linalg.aslinearoperator(h)
+        g = krylov.solve_Hessenberg(h, 1)
+        c = self._unit_vector(0)
+        np.testing.assert_almost_equal(np.dot(h, g), c)
 
     def test_gmres(self):
-        N = 5
-        rand = np.random.rand(N, N)
-        A = linalg.LinearOperator((N, N), matvec=lambda x: np.dot(rand, x), dtype=np.float64)
-        x = np.random.rand(N)
+        A = self._random_linop()
+        x = self._random_vector()
         b = A * x
         y = krylov.gmres(A, b)
         np.testing.assert_almost_equal(y, x)
