6477768e3040ed43fb10072730b84c29bef1e949
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-09 19:15:11 +0900

    Change manual listing of submodules to automatic

--- continuate/__init__.py
+++ continuate/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import importlib
+import os.path as op
+from glob import glob
 
-__all__ = ["linalg", "single_parameter"]
+__all__ = [op.basename(f)[:-3]
+           for f in glob(op.join(op.dirname(__file__), "*.py"))
+           if op.basename(f) != "__init__.py"]
 
 for m in __all__:
     importlib.import_module("continuate." + m)
