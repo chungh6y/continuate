320182cf72ff96294c20edac510b9fb5fb76d839
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-15 22:44:32 +0900

    Version up (v0.2.1)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name="continuate",
-      version="0.2.0",
+      version="0.2.1",
       description="parameter continuation of continuous function",
       author="Toshiki Teramura",
       author_email="toshiki.teramura+continuate@gmail.com",
