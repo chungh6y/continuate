b4f072fd4cd37bd748faf98bb91e230fbda25756
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-15 21:41:15 +0900

    Implement new parameter setting

--- continuate/__init__.py
+++ continuate/__init__.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import importlib
 import os.path as op
 from glob import glob
+from importlib import import_module
 
 
 __all__ = [op.basename(f)[:-3]
            for f in glob(op.join(op.dirname(__file__), "*.py"))
-           if op.basename(f) != "__init__.py"]
+           if op.basename(f) != "__init__.py"
+           if not op.basename(f).startswith("test_")]
 
-for m in __all__:
-    importlib.import_module("continuate." + m)
+_mods = [import_module("continuate." + m) for m in __all__]
+
+
+def get_default_options():
+    """ get default options as a dict """
+    cfg = {}
+    for m in _mods:
+        if hasattr(m, "default_options"):
+            cfg.update(m.default_options)
+    return cfg
--- continuate/newton.py
+++ continuate/newton.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+""" Basic Newton methods using Krylov subspace method.
+
+Options
+--------
+newton_tol : float
+    Tolerrance of Newton step
+newton_maxiter : int
+    Max iteration number of Newton method
+jacobi_alpha : float
+    Infinitesimal for calculating Jacobian matrix
+trusted_region : float
+    Radius in model trusted region approach
+hook_maxiter : int
+    Max iteration number of hook step
+hook_tol : int
+    Relative tolerance of hook step iteration
+
+Their default values are set in :py:data:`.default_options`
+"""
 
 import numpy as np
 import scipy.sparse.linalg as linalg
@@ -6,8 +25,21 @@ from itertools import count as icount
 from .logger import Logger
 from . import krylov
 
+default_options = {
+    "newton_tol": 1e-7,
+    "newton_maxiter": 100,
+    "jacobi_alpha": 1e-7,
+    "trusted_region": 1e-1,
+    "hook_maxiter": 100,
+    "hook_tol": 0.1,
+}
+""" default values of options
 
-def Jacobi(func, x0, alpha=1e-7, fx=None):
+You can get these values through :py:func:`continuate.get_default_options`
+"""
+
+
+def Jacobi(func, x0, jacobi_alpha, fx=None, **opt):
     """
     Jacobi oprator :math:`DF(x0)`, where
 
@@ -30,7 +62,7 @@ def Jacobi(func, x0, alpha=1e-7, fx=None):
     >>> import numpy as np
     >>> f = lambda x: np.array([x[1]**2, x[0]**2])
     >>> x0 = np.array([1, 2])
-    >>> J = Jacobi(f, x0)
+    >>> J = Jacobi(f, x0, 1e-7)
 
     """
     if fx is None:
@@ -38,7 +70,7 @@ def Jacobi(func, x0, alpha=1e-7, fx=None):
 
     def wrap(v):
         norm = np.linalg.norm(v)
-        r = alpha / norm
+        r = jacobi_alpha / norm
         return (func(x0 + r * v) - fx) / r
 
     return linalg.LinearOperator(
@@ -53,12 +85,12 @@ class Hessian(object):
 
     logger = Logger(__name__, "Hessian")
 
-    def __init__(self, func, x0, alpha=1e-7):
+    def __init__(self, func, x0, jacobi_alpha, **opt):
         self.fx0 = func(x0)
-        self.A = Jacobi(func, x0, fx=self.fx0, alpha=1e-7)
+        self.A = Jacobi(func, x0, fx=self.fx0, jacobi_alpha=1e-7)
         self.f = func
         self.x0 = x0
-        self.alpha = alpha
+        self.alpha = jacobi_alpha
 
     def __call__(self, v):
         fxv = self.f(self.x0 + v)
@@ -102,7 +134,7 @@ class Hessian(object):
             r = r * np.sqrt(eps / e)
 
 
-def newton(func, x0, ftol=1e-5, maxiter=100, inner_tol=1e-6):
+def newton(func, x0, newton_tol, newton_maxiter, **opt):
     """
     solve multi-dimensional equation `F(x) = 0` using Newton-Krylov method.
 
@@ -126,22 +158,22 @@ def newton(func, x0, ftol=1e-5, maxiter=100, inner_tol=1e-6):
 
     """
     logger = Logger(__name__, "Newton")
-    for t in range(maxiter):
+    for t in range(newton_maxiter):
         fx = func(x0)
         res = np.linalg.norm(fx)
         logger.info({
             "count": t,
             "residual": res,
         })
-        if res <= ftol:
+        if res <= newton_tol:
             return x0
-        A = Jacobi(func, x0, fx=fx)
-        dx = krylov.gmres(A, -fx, eps=inner_tol)
+        A = Jacobi(func, x0, fx=fx, **opt)
+        dx = krylov.gmres(A, -fx, **opt)
         x0 = x0 + dx
     raise RuntimeError("Not convergent (Newton)")
 
 
-def hook_step(A, b, r, nu=0, maxiter=100, e=0.1):
+def hook_step(A, b, trusted_region, hook_maxiter, hook_tol, nu=0, **opt):
     """
     optimal hook step based on trusted region approach
 
@@ -171,6 +203,7 @@ def hook_step(A, b, r, nu=0, maxiter=100, e=0.1):
 
     """
     logger = Logger(__name__, "Hook step")
+    r = trusted_region
     logger.debug({"nu0": nu, "trusted_region": r, })
     I = np.matrix(np.identity(len(b), dtype=b.dtype))
     AA = np.dot(A.T, A)
@@ -178,7 +211,7 @@ def hook_step(A, b, r, nu=0, maxiter=100, e=0.1):
     xi = np.linalg.solve(AA, Ab)
     if np.linalg.norm(xi) < r:
         return xi, 0
-    for t in range(maxiter):
+    for t in range(hook_maxiter):
         xi = np.linalg.solve(AA+nu*I, Ab)
         xi_norm = np.linalg.norm(xi)
         Psi = xi_norm - r
@@ -186,7 +219,7 @@ def hook_step(A, b, r, nu=0, maxiter=100, e=0.1):
             "count": t,
             "1-|xi|/r": Psi / r,
         })
-        if abs(Psi) < e * r:
+        if abs(Psi) < hook_tol * r:
             return xi, nu
         dPsi = -np.dot(xi, np.linalg.solve(AA+nu*I, xi)) / xi_norm
         nu = nu - (xi_norm*Psi) / (r*dPsi)
@@ -202,7 +235,7 @@ def hook_step(A, b, r, nu=0, maxiter=100, e=0.1):
     raise RuntimeError("Not convergent (hook-step)")
 
 
-def newton_krylov_hook_gen(func, x0, r=1e-2, inner_tol=1e-6, **kwds):
+def newton_krylov_hook_gen(func, x0, trusted_region, **opt):
     """ Generator of Newton-Krylov-hook iteration
 
     Parameters
@@ -225,30 +258,31 @@ def newton_krylov_hook_gen(func, x0, r=1e-2, inner_tol=1e-6, **kwds):
             "residual": res,
         })
         yield x0, res, fx
-        A = Jacobi(func, x0, fx=fx)
+        A = Jacobi(func, x0, fx=fx, **opt)
         b = -fx
-        H, V = krylov.arnoldi(A, b, eps=inner_tol)
+        H, V = krylov.arnoldi(A, b, **opt)
         g = krylov.solve_Hessenberg(H, np.linalg.norm(b))
         dx = np.dot(V[:, :len(g)], g)
         dx_norm = np.linalg.norm(dx)
         logger.info({"|dx|": dx_norm, })
-        if dx_norm < r:
+        if dx_norm < trusted_region:
             logger.info('in Trusted region')
             x0 = x0 + dx
         else:
             logger.info('hook step')
             beta = np.zeros(H.shape[0])
             beta[0] = np.linalg.norm(b)
-            xi, nu = hook_step(H, beta, r, nu=nu)
+            xi, nu = hook_step(H, beta, trusted_region, nu=nu, **opt)
             dx = np.dot(V[:, :len(xi)], xi)
             x0 = x0 + dx
 
 
-def newton_krylov_hook(func, x0, r=1e-2, ftol=1e-5, inner_tol=1e-6,
-                       maxiter=100, **kwds):
-    gen = newton_krylov_hook_gen(func, x0, r=r, inner_tol=inner_tol, **kwds)
+def newton_krylov_hook(func, x0, **opt):
+    tol = opt["newton_tol"]
+    maxiter = opt["newton_maxiter"]
+    gen = newton_krylov_hook_gen(func, x0, **opt)
     for t, (x, res, _) in enumerate(gen):
-        if res < ftol:
+        if res < tol:
             return x
         if t > maxiter:
             raise RuntimeError("Not Convergent (Newton-Krylov-hook)")
