88ebf3e1e63039872615009ec257ff51903a8a95
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-15 22:47:12 +0900

    Not to test get_default_options

--- continuate/__init__.py
+++ continuate/__init__.py
@@ -14,14 +14,7 @@ _mods = [import_module("continuate." + m) for m in __all__]
 
 
 def get_default_options():
-    """ get default options as a dict
-
-    Examples
-    ---------
-    >>> get_default_options()
-    {'krylov_maxiter': 1000.0, 'krylov_tol': 1e-09, 'newton_maxiter': 100, 'jacobi_alpha': 1e-07, 'newton_tol': 1e-07, 'hook_maxiter': 100, 'hook_tol': 0.1, 'trusted_region': 0.1}
-
-    """
+    """ get default options as a dict """
     cfg = {}
     for m in _mods:
         if hasattr(m, "default_options"):
