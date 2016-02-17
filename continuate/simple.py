9f4e3aac0c08f437b58bde1570a6094dbb655620
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-02-17 22:32:24 +0900

    Add simple continuation

---
+++ continuate/simple.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+""" Simple parameter continuation """
+
+from . import newton
+from .logger import Logger
+from itertools import count as icount
+
+
+def continuation(func, x, mu, delta, **opt):
+    """
+
+    Parameters
+    -----------
+    func : (numpy.array, float) -> numpy.array
+        :math:`F(x, \mu)`
+        :code:`func(x, mu)` must have same dimension of :code:`x`
+    x : numpy.array
+        Initial point of continuation, and satisfies :math:`F(x, \mu) = 0`
+    mu : float
+        Initial parameter of continuation, and satisfies :math:`F(x, \mu) = 0`
+    delta : float
+        step length of continuation.
+        To decrease the parameter, you should set negative value.
+
+    """
+    logger = Logger(__name__, "Continuation")
+    for t in icount():
+        mu += delta
+        f = lambda x: func(x, mu)
+        x = newton.newton_krylov_hook(f, x, **opt)
+        logger.info({
+            "count": t,
+            "mu": mu,
+        })
+        yield x, mu
