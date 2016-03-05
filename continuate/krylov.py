933837379b69cff8903256185f1e9f69fe09d35f
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-03-05 18:09:00 +0900

    Replace gmres and remove old codes

--- continuate/krylov.py
+++ continuate/krylov.py
@@ -19,9 +19,7 @@ Their default values are set in :py:data:`.default_options`
 """
 
 import numpy as np
-from numpy import dot
 from numpy.linalg import norm
-from itertools import count as icount
 from .misc import Logger
 from . import qr, exceptions
 
@@ -64,8 +62,8 @@ def arnoldi_common(A, r, krylov_tol=default_options["krylov_tol"],
     raise exceptions.MaxIteration("arnoldi_common")
 
 
-def gmres_gen(A, b, x0=None, krylov_tol=default_options["krylov_tol"],
-              krylov_maxiter=default_options["krylov_maxiter"]):
+def gmres(A, b, x0=None, krylov_tol=default_options["krylov_tol"],
+          krylov_maxiter=default_options["krylov_maxiter"]):
     """ Solve linear equations :math:`Ax=b` by GMRES
 
     Parameters
@@ -84,7 +82,7 @@ def gmres_gen(A, b, x0=None, krylov_tol=default_options["krylov_tol"],
     >>> A = aslinearoperator(random((5, 5)))
     >>> x = random(5)
     >>> b = A*x
-    >>> ans = gmres_gen(A, b)
+    >>> ans = gmres(A, b)
     >>> np.allclose(ans, x)
     True
 
@@ -116,98 +114,3 @@ def gmres_gen(A, b, x0=None, krylov_tol=default_options["krylov_tol"],
         H[:n+1, n] = h
     y = np.linalg.solve(H, g[:-1])
     return np.dot(V, y)
-
-
-class Arnoldi(object):
-    """ Construct Krylov subspace (Arnoldi process)
-
-    .. math::
-        AV_n = V_n H_n + re^T
-
-    Attributes
-    -----------
-    residual : float
-        The residual of Arnoldi Process
-    matrix_norm : float
-        Approximated (projected) matrix norm of `A`
-
-    """
-
-    logger = Logger(__name__, "Arnoldi")
-
-    def __init__(self, A, b, krylov_tol, **opt):
-        self.A = A
-        self.ortho = qr.MGS(eps=krylov_tol)
-        self.ortho(b)
-        self.eps = krylov_tol
-        self.coefs = []
-        self._calc()
-
-    def __iter__(self):
-        return self.ortho.__iter__()
-
-    def __getitem__(self, i):
-        return self.ortho[i]
-
-    def _calc(self):
-        """ Main process of Arnoldi process """
-        self.residual = 1.0
-        self.matrix_norm = 0.0
-        for c in icount():
-            v = self.ortho[-1]
-            Av = self.A * v
-            self.matrix_norm = max(self.matrix_norm, norm(Av))
-            coef = self.ortho(Av)
-            self.residual *= coef[-1]
-            self.logger.info({
-                "count": c,
-                "residual": self.residual,
-            })
-            self.coefs.append(coef)
-            if self.residual < self.eps:
-                self.logger.info({"matrix_norm": self.matrix_norm, })
-                return
-
-    def basis(self):
-        return np.stack(self).T
-
-    def projected_matrix(self):
-        N = len(self.coefs)
-        H = np.zeros((N, N))
-        for i, c in enumerate(self.coefs):
-            n = min(N, len(c))
-            H[:n, i] = c[:n]
-        return H
-
-    def __call__(self):
-        return self.projected_matrix(), self.basis()
-
-
-def arnoldi(A, b, **opt):
-    O = Arnoldi(A, b, **opt)
-    return O()
-
-
-def solve_Hessenberg(H, b):
-    N = len(H)
-    g = np.zeros((N, 1))
-    g[0, 0] = b
-    if N == 1:
-        return g[:, 0] / H[0, 0]
-    Hg = np.concatenate((H, g), axis=1)
-    for i in range(N):
-        Hg[i, i+1:] /= Hg[i, i]
-        Hg[i, i] = 1
-        if i == N-1:
-            break
-        Hg[i+1, i:] -= Hg[i+1, i] * Hg[i, i:]
-    for i in reversed(range(1, N)):
-        Hg[:i, N] -= Hg[i, N] * Hg[:i, i]
-        Hg[:i, i] = 0
-    return Hg[:, N]
-
-
-def gmres(A, b, **opt):
-    H, V = arnoldi(A, b, **opt)
-    g = solve_Hessenberg(H, norm(b))
-    return dot(V[:, :len(g)], g)
